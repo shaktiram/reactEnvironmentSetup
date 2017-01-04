07768f8b7af6f8c19127b2ce12a691fbcf57e5b1
Jagatheeswaran T S <jagatheeswaran.t.s@gmail.com>
2017-01-04 14:31:52 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.src(['./package.json'])
     .pipe(install());
 
 gulp.task('lint', () => {
-    return gulp.src(['./container/main.js', '!node_modules/**'])
+    return gulp.src(['./app/main.js', '!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
